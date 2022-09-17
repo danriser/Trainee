650661791f497db014c7f02df78ed58dcd4f5abb
danriser <79719488+danriser@users.noreply.github.com>
2022-09-17 10:07:29 -0300

    Delete lista1_questao4.rb

--- programs/lista1_questao4.rb
+++
@@ -1,8 +0,0 @@
-print "Pedros da Struct:"
-pe = gets.split(",") 
-for nome in pe:
-    
-print "Arthures da Struct:"
-ar = gets.split(",")
-guerreiros = {pedros=>(pe),arthures=>ar }
-puts guerreiros
